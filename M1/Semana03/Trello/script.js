b45f45e406e8c47ef4dcc202b1f608f80ec5f09f
Yan Esteves <yan.esteves@audaces.com>
2024-08-06 20:52:21 -0300

    Semana 03

---
+++ M1/Semana03/Trello/script.js
@@ -0,0 +1,20 @@
+console.log("Olá, Mundo. Mensagem do JS.")
+console.log("Mensagem 2")
+
+// alert("Mensagem para o usuário")
+
+const variavelConstante = "Florianópolis"
+// variavelConstante = "São Paulo" // não permitido por ser constante
+
+// let || var
+let variavelDinamica = "Rio de Janeiro"
+variavelDinamica = "São Paulo"
+
+alert(variavelDinamica)
+
+function somarDoisNumeros(numero1, numero2) {
+    const resultado = numero1 + numero2
+    console.log(resultado)
+}
+
+somarDoisNumeros(8, 64)
